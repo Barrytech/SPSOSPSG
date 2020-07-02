3ed0704e8506fc2002cf203ba60c4d3290f1a55f
Elias <eliasreta@icloud.com>
2020-07-02 14:16:53 -0400

    Did a lot lol, but what works is the arraylist with all of the new project's info is being displayed by the server

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -11,30 +11,56 @@ import java.lang.String;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import java.util.HashMap;
 
 
 
-@WebServlet("/project-list")
-public class DataServlet extends HttpServlet { //class
+@WebServlet("/project-info")
+public class DataServlet extends HttpServlet { //ADD YOUR FUNCTIONS IN HERE
 //
 //ELIAS
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //response.setContentType("text/html");
+        String userName = request.getParameter("userName"); //request is getting the info from the html through the javascript
+        String projectName = request.getParameter("projectName");
+        String projectDescription = request.getParameter("projectDescription");
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Entity newProjectEntity = new Entity("Project"); //"Project" is the entity 'kind' I'm creating
         newProjectEntity.setProperty("userName", userName); //entity properties have a (key, value), and are stored in datastore
-        newProjectEntity.setProperty("project", projectName);
+        newProjectEntity.setProperty("projectName", projectName);
         newProjectEntity.setProperty("projectDescription", projectDescription);
         datastore.put(newProjectEntity); //stores the entity
+        response.getWriter().println("Save Successful");
     }
-    //Going through the database and posting the projects
-    Query query = new Query("Project").addSort("userName". SortDirection.DESCENDING); //allows us to query through all the entities with kind "Project" and Property "userName"
-    PreparedQuery results = datasore.prepare(query); //resuls contains all the entities in Datastore with that kind
-    for (Entity entity: results.asIterable()) {
-        entity.getProperty(); //gets the properties set on each entity.  
-    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        //Going through the database and posting the projects
+        Query query = new Query("Project").addSort("userName", SortDirection.DESCENDING); //allows us to query through all the entities with kind "Project" and Property "userName"
+        PreparedQuery results = datastore.prepare(query); //resuls contains all the entities in Datastore with that kind
+        ArrayList<HashMap<String, String>> projects = new ArrayList<>(); //this is an arrayList of HashMaps, each of which contain a specific project's details
+        for (Entity entity: results.asIterable()) {
+            String userName = (String) entity.getProperty("userName"); //gets the properties set on each entity. 
+            String projectName = (String) entity.getProperty("projectName");
+            String projectDescription = (String) entity.getProperty("projectDescription");
+
+            HashMap<String, String> projectInfo = new HashMap();
+            projectInfo.put("userName", userName);
+            projectInfo.put("projectName", projectName);
+            projectInfo.put("projectDescription", projectDescription);
+            projects.add(projectInfo);
+        }
+        Gson gson = new Gson();
+        response.setContentType("application/json;");
+        response.getWriter().println(gson.toJson(projects)); //response contains all of the project's information in "application/json" form
+    } 
 //ELIAS
 //
 
