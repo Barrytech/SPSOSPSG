71861cedb6151a1870c87b8310ec523bbf4b2d8b
eliasreta <55664012+eliasreta@users.noreply.github.com>
2020-07-15 13:56:45 -0400

    Update projectList.js

--- src/main/webapp/projectList/projectList.js
+++ src/main/webapp/projectList/projectList.js
@@ -2,7 +2,7 @@ function loadProjectInfo() {
       const projectInfoPromise = fetch('/project-info').then(response => response.json()); //we're fetching information from the 'project-list' server
       projectInfoPromise.then((response) => { //fecth() returns a 'promise' object, saying that when the server sends back information, then do "X"
         for (let i = 0; i < response.length; i++) { //response = the arrayList in Json
-            let projectInfo = response[i]; //this is getting a hashmap
+            let projectInfo = response[i];
             let project = document.createElement('project-post');
             let postUserName = document.createElement('post-user-name');
             let postProjectName = document.createElement('post-project-name');
